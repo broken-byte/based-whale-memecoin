f021c94f5117e01bc36f6da18f2512ec566c28be
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-06-18 18:00:47 -0700

    Deployed to Base Sepolia, modified hardhat config, modified deploy script for Base Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const ALCHEMY_BASE_MAINNET_API_KEY: string = process.env.ALCHEMY_API_KEY!!;
+const BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY: string = process.env.BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY!!;
+const ALCHEMY_BASE_SEPLOYIA_API_KEY: string = process.env.ALCHEMY_BASE_SEPOLIA_API_KEY!!;
+const BASESCAN_BASED_WHALE_API_KEY: string = process.env.BASESCAN_BASED_WHALE_API_KEY!!;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     hardhat: {
       forking: {
-        url: `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        url: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_BASE_MAINNET_API_KEY}`,
         blockNumber: 15972484
-      }
-    }
-  }
+      },
+    },
+    // Base Sepolia
+    sepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_BASE_SEPLOYIA_API_KEY}`,
+      accounts: [BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: BASESCAN_BASED_WHALE_API_KEY,
+  },
 };
 
 export default config;
--- ignition/modules/BasedWhale.ts
+++ ignition/modules/BasedWhale.ts
@@ -3,11 +3,18 @@ import { ethers } from "hardhat";
 
 const BasedWhaleModule = buildModule("BasedWhale", (m) => {
   const oneBillionCappedSupply = BigInt(1_000_000_000);
-  const initialOwnerAddress = "0x55B554EFaDdB6a229A3785cc535779E54be5308c" // BrokenByte;
+  const initialOwnerAddress = "0x566bD9Df983BfEFf836A2C3b644553e6A80850Fa" // BrokenByte Engineering Deployer;
   const marketingMultiSigAddress = "0x65D23D5956b2F7E519df94dffA21ff3b078e7FE0";
   const exchangeMultiSigAddress1 = "0xeBa74571b3B7703580f908a6A9035EB930C4F37D";
   const exchangeMultiSigAddress2 = "0x6ca7304B0871F36Cd3cDF429b928a57a939d86A1";
   const uniswapV2RouterBaseAddress = "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24";
+  const uniswapV2RouterBaseSepoliaAddress = "0x1689E7B1F10000AE47eBfE339a4f69dECd19F602";
+  const liquidityETHProvisionMainnet = "1.004";
+  const liquidityETHProvisionTestnet = "0.025";
+
+  const isTestnet: Boolean = true;
+  const uniswapRouterAddressFinal = isTestnet ? uniswapV2RouterBaseSepoliaAddress : uniswapV2RouterBaseAddress;
+  const liquidityETHProvisionFinal = isTestnet ? liquidityETHProvisionTestnet : liquidityETHProvisionMainnet;
 
   // 1. Deploy contract
   const basedWhale = m.contract(
@@ -25,8 +32,8 @@ const BasedWhaleModule = buildModule("BasedWhale", (m) => {
   m.call(
     basedWhale,
     "initializeUniswapLiquidity",
-    [uniswapV2RouterBaseAddress],
-    { value: ethers.parseEther("1.004") }
+    [uniswapRouterAddressFinal],
+    { value: ethers.parseEther(liquidityETHProvisionFinal) }
   );
 
   return { basedWhale };
