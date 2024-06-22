58121247a55cf4acca3b4f109c57494e7f52c062
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-06-21 19:16:38 -0700

    Fixed configs so that CI/CD works

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,19 @@ import "@nomicfoundation/hardhat-toolbox";
 import "./tasks/LockBasedWhaleLiquidity";
 import "@nomicfoundation/hardhat-verify";
 
+const isPreppedForTestnetAndMainnetDeployments: Boolean = true;
+
 const ALCHEMY_BASE_MAINNET_API_KEY: string = process.env.ALCHEMY_API_KEY!!;
-const BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY: string = process.env.BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY!!;
-const ALCHEMY_BASE_SEPLOYIA_API_KEY: string = process.env.ALCHEMY_BASE_SEPOLIA_API_KEY!!;
+
+// I dont trust Github's servers THAT much so I will not be adding my private key/base api key to the repo 
+// secrets mappings after testnet/mainnet deployment.
+const BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY: string =
+  isPreppedForTestnetAndMainnetDeployments ?
+  "" : process.env.BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY!!;
+const ALCHEMY_BASE_SEPLOYIA_API_KEY: string = 
+  isPreppedForTestnetAndMainnetDeployments ?
+  "" : process.env.ALCHEMY_BASE_SEPOLIA_API_KEY!!;
+
 const BASESCAN_BASED_WHALE_API_KEY: string = process.env.BASESCAN_BASED_WHALE_API_KEY!!;
 
 const config: HardhatUserConfig = {
